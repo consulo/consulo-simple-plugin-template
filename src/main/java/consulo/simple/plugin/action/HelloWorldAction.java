7ea0253f6726151ddb71946cc603424cc9da64f5
Valery Semenchuk <vistall.valeriy@gmail.com>
2023-01-24 15:09:17 +0400

    migrated to Consulo 3 API

--- src/main/java/consulo/simple/plugin/action/HelloWorldAction.java
+++ src/main/java/consulo/simple/plugin/action/HelloWorldAction.java
@@ -1,23 +1,40 @@
 package consulo.simple.plugin.action;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import consulo.simple.plugin.localize.SimpleLocalize;
+import consulo.annotation.component.ActionImpl;
+import consulo.annotation.component.ActionParentRef;
+import consulo.annotation.component.ActionRef;
+import consulo.simple.plugin.icon.SimplePluginIconGroup;
 import consulo.ui.Alerts;
 import consulo.ui.annotation.RequiredUIAccess;
+import consulo.ui.ex.action.AnAction;
+import consulo.ui.ex.action.AnActionEvent;
+import consulo.ui.image.Image;
+import consulo.simple.plugin.localize.SimpleLocalize;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * @author VISTALL
  * @since 2020-10-29
  */
+@ActionImpl(id = "SimpleHelloWorld", parents =
+{
+	@ActionParentRef(@ActionRef(id = "ToolsMenu"))
+})
 public class HelloWorldAction extends AnAction
 {
+	@Nullable
+	@Override
+	protected Image getTemplateIcon()
+	{
+		return SimplePluginIconGroup.plugin();
+	}
+
 	@RequiredUIAccess
 	@Override
-	public void actionPerformed(@Nonnull AnActionEvent anActionEvent)
+	public void actionPerformed(@Nonnull AnActionEvent e)
 	{
-		Alerts.okInfo(SimpleLocalize.SimpleHelloWorldMessage().get()).showAsync();
+		Alerts.okInfo(SimpleLocalize.simplehelloworldMessage()).showAsync();
 	}
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author VISTALL
+ * @since 24/01/2023
+ */
+module consulo.simple.plugin
+{
+	requires consulo.ide.api;
+}
