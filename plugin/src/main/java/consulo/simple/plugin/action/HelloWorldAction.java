cb23178df64aed8851eb85b7261f72baae14874e
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-01-04 18:48:38 +0200

    actualization template plugin

--- plugin/src/main/java/consulo/simple/plugin/action/HelloWorldAction.java
+++ plugin/src/main/java/consulo/simple/plugin/action/HelloWorldAction.java
@@ -4,37 +4,26 @@ import consulo.annotation.component.ActionImpl;
 import consulo.annotation.component.ActionParentRef;
 import consulo.annotation.component.ActionRef;
 import consulo.simple.plugin.icon.SimplePluginIconGroup;
+import consulo.simple.plugin.localize.SimpleLocalize;
 import consulo.ui.Alerts;
 import consulo.ui.annotation.RequiredUIAccess;
 import consulo.ui.ex.action.AnAction;
 import consulo.ui.ex.action.AnActionEvent;
-import consulo.ui.image.Image;
-import consulo.simple.plugin.localize.SimpleLocalize;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import jakarta.annotation.Nonnull;
 
 /**
  * @author VISTALL
  * @since 2020-10-29
  */
-@ActionImpl(id = "SimpleHelloWorld", parents =
-{
-	@ActionParentRef(@ActionRef(id = "ToolsMenu"))
-})
-public class HelloWorldAction extends AnAction
-{
-	@Nullable
-	@Override
-	protected Image getTemplateIcon()
-	{
-		return SimplePluginIconGroup.plugin();
-	}
+@ActionImpl(id = "SimpleHelloWorld", parents = @ActionParentRef(@ActionRef(id = "ToolsMenu")))
+public class HelloWorldAction extends AnAction {
+    public HelloWorldAction() {
+        super(SimpleLocalize.actionSimplehelloworldText(), SimpleLocalize.actionSimplehelloworldDescription(), SimplePluginIconGroup.plugin());
+    }
 
-	@RequiredUIAccess
-	@Override
-	public void actionPerformed(@Nonnull AnActionEvent e)
-	{
-		Alerts.okInfo(SimpleLocalize.simplehelloworldMessage()).showAsync();
-	}
+    @RequiredUIAccess
+    @Override
+    public void actionPerformed(@Nonnull AnActionEvent e) {
+        Alerts.okInfo(SimpleLocalize.simplehelloworldMessage()).showAsync();
+    }
 }
--- plugin/src/main/java/module-info.java
+++ plugin/src/main/java/module-info.java
@@ -2,7 +2,6 @@
  * @author VISTALL
  * @since 24/01/2023
  */
-module consulo.simple.plugin
-{
-	requires consulo.ide.api;
+module consulo.simple.plugin {
+    requires consulo.ide.api;
 }
